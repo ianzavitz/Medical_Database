9220f143707775d35ef86eaefd0eaf158dc3d392
ianzavitz <ianzavitz@gmail.com>
2018-12-10 11:53:35 -0600

    added a line in models.py because it was broken

--- App/flaskDemo/models.py
+++ App/flaskDemo/models.py
@@ -1,6 +1,11 @@
 from datetime import datetime
 from flaskDemo import db, login_manager
 from flask_login import UserMixin
+from functools import partial
+from sqlalchemy import orm
+
+
+db.Model.metadata.reflect(db.engine)
 
 
 @login_manager.user_loader
